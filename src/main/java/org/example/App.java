8aae2a35558bcdce06d94ce18c3c6ff855d77226
Pedro Henrique Sotto-Mayor Pereira da Silva <itapeu16@hotmail.com>
2021-09-13 23:19:50 -0700

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner sc= new Scanner(System.in);
+        String answer="";
+        System.out.print("Is the car silent when you turn the key? ");
+        answer=sc.nextLine();
+
+        if(answer.equals("y")){
+            System.out.print("Are the battery terminals corroded? ");
+            answer=sc.nextLine();
+            if(answer.equals("y")){
+                System.out.print("Clean terminals and try starting again.\n ");
+            }
+            else{
+                System.out.print("Replace cables and try again.\n");
+            }
+        }
+        else{
+            System.out.print("Does the car make a slicking noise? ");
+            answer=sc.nextLine();
+            if(answer.equals("y")){
+                System.out.print("Replace the battery.\n");
+            }
+            else{
+                System.out.print("Does the car crank up but fail to start? ");
+                answer=sc.nextLine();
+                if(answer.equals("y")){
+                    System.out.print("Check spark plug connections.\n");
+                }
+                else{
+                    System.out.print("Does the engine start and then die? ");
+                    answer=sc.nextLine();
+                    if(answer.equals("y")){
+                        System.out.print("Does you car have fuel injection? ");
+                        answer=sc.nextLine();
+                        if(answer.equals("y")){
+                            System.out.print("Get it in for service.\n");
+                        }
+                        else{
+                            System.out.print("Check to ensure the choke is opening and closing.\n");
+                        }
+                    }
+                    else{
+                        System.out.print("This should not be possible.\n");
+                    }
+                }
+            }
+
+        }
+    }
+}
